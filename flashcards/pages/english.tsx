8848f32b1f969ae4d660f992e3be3caebc646191
dianexjeong <zuphyr14@gmail.com>
2022-12-30 23:36:39 +0900

    [feat] create flashcard in hover flip mode

--- flashcards/pages/english.tsx
+++ flashcards/pages/english.tsx
@@ -3,13 +3,14 @@ import utilStyles from "../styles/utils.module.css";
 import englishKorean from "../words/englishKorean";
 import englishWords from "../words/englishWords";
 import getRandomInt from "../components/random";
+import Card from "../components/flipcard";
 
 export default function English() {
   var num = getRandomInt();
   return (
     <Layout>
       <h1 className={utilStyles.heading2Xl}>Let's study English!</h1>
-      <p>{num}</p>
+      <div>{Card(englishWords[num], englishKorean[num])}</div>
     </Layout>
   );
 }
