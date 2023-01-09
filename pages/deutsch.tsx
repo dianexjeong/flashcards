6bccfdb854c737fc6a5daa16778197716be3c499
dianexjeong <zuphyr14@gmail.com>
2023-01-09 22:32:21 +0900

    [feat] connect air quality api to english and deutsch

--- pages/deutsch.tsx
+++ pages/deutsch.tsx
@@ -1,13 +1,27 @@
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import getRandomInt from "../components/random";
+import axios from "axios";
 
-const Deutsch = () => {
+const API_KEY = process.env.AIR_API_KEY;
+
+const Deutsch = ({ airQuality }: { airQuality: string }) => {
   return (
-    <Layout deutsch>
+    <Layout deutsch air={airQuality}>
       <h1 className={utilStyles.heading2Xl}>Lass uns Deutsch lernen!</h1>
     </Layout>
   );
 };
 
 export default Deutsch;
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/RealtimeCityAir/1/5/동남권/서초구`
+  );
+  const airQuality = res.data.RealtimeCityAir.row[0].IDEX_NM;
+
+  return {
+    props: { airQuality },
+  };
+}
--- pages/english.tsx
+++ pages/english.tsx
@@ -1,10 +1,13 @@
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Flashcard from "../components/Flashcard";
+import axios from "axios";
 
-const English = () => {
+const API_KEY = process.env.AIR_API_KEY;
+
+const English = ({ airQuality }: { airQuality: string }) => {
   return (
-    <Layout>
+    <Layout air={airQuality}>
       <h1 className={utilStyles.heading2Xl}>Let's study English!</h1>
       <Flashcard />
     </Layout>
@@ -12,3 +15,14 @@ const English = () => {
 };
 
 export default English;
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/RealtimeCityAir/1/5/동남권/서초구`
+  );
+  const airQuality = res.data.RealtimeCityAir.row[0].IDEX_NM;
+
+  return {
+    props: { airQuality },
+  };
+}
