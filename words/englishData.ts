b36a4ecfae933191b9ed54a237dc78fe6a69b6e3
dianexjeong <zuphyr14@gmail.com>
2023-01-10 17:40:08 +0900

    [feat] add json data

---
+++ words/englishData.ts
@@ -0,0 +1,17 @@
+export const englishData = [
+  {
+    id: 0,
+    word: "abandon",
+    def: "버리다",
+  },
+  {
+    id: 1,
+    word: "abate",
+    def: "감소시키다",
+  },
+  {
+    id: 2,
+    word: "a view range of",
+    def: "다양한",
+  },
+];
---
+++ words/germanData.ts
@@ -0,0 +1,17 @@
+export const germanData = [
+  {
+    id: 0,
+    word: "die Geschichte",
+    def: "역사",
+  },
+  {
+    id: 1,
+    word: "das Ziel",
+    def: "목적",
+  },
+  {
+    id: 2,
+    word: "tepezieren",
+    def: "도배하다",
+  },
+];
